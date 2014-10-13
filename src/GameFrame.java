20530b41899d4ae5ff6364f6cd2a75fe16da4a7f
arianne-ao <arianne_ong0807@yahoo.com>
2014-10-13 13:40:41 -0800

    inital commit

---
+++ src/GameFrame.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics2D;
+
+import com.golden.gamedev.Game;
+
+
+public class GameFrame extends Game {
+
+	public void initResources() {
+		
+	}
+
+	public void render(Graphics2D arg0) {
+		
+	}
+
+	public void update(long arg0) {
+		
+	}
+
+}
---
+++ src/TetrisGame.java
@@ -0,0 +1,14 @@
+import java.awt.Dimension;
+
+import com.golden.gamedev.GameLoader;
+
+public class TetrisGame {
+	
+	public static void main(String[] args) {
+		GameFrame gt = new GameFrame();
+		GameLoader game = new GameLoader();
+		game.setup(gt, new Dimension(640, 640), false); // false - window format , true - full screen
+		game.start();		
+	}
+
+}
