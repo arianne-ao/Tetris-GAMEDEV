9bebed2523916e1e93f4fbb99fa27b116402797a
arianne-ao <arianne_ong0807@yahoo.com>
2014-10-27 13:42:47 -0800

    add piece freq class

---
+++ src/PieceFrequency.java
@@ -0,0 +1,15 @@
+
+public class PieceFrequency {
+
+	String name;
+	Tetromino t;
+	int freq;
+	
+	public PieceFrequency(String name, Tetromino t, int freq) {
+		this.name = name;
+		this.t = t;
+		this.freq = freq;
+		
+		
+	}
+}
